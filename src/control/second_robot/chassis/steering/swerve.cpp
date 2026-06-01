353534152a0ec506b0415e05e5a8f59262c8cf04
resetinging <yaolux44@gamil.com>
2026-06-01 16:10:51 +0800

    a

---
+++ src/control/second_robot/chassis/steering/swerve.cpp
@@ -0,0 +1,13 @@
+#include "control/second_robot/chassic/streering/swerve.h"
+
+using basic::control::second_robot{
+namespace{
+
+using basic::control::stopcontrol;
+using basic::control::velocitycontrol;
+using basic::hardware::second_robot::RobotHardware;
+using basic::hardware::second_robot::RobotState;
+
+
+}//namespace
+}//basic::control::second_robot
--- src/control/second_robot/chassis/streering/swerve.cpp
+++
@@ -1,7 +0,0 @@
-#include "control/second_robot/chassic/streering/swerve.h"
-
-using basic::control::second_robot{
-namespace{
-    
-}
-}
